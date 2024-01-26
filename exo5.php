a574ed7e49ea3e37e6e5028ca6fce2a53b2d1a8b
JefG67 <jefguilpain@gmail.com>
2024-01-26 11:55:25 +0100

    exo 2-5

---
+++ exo5.php
@@ -0,0 +1,13 @@
+<h1>Exercice 5</h1>
+
+<p>Créer une fonction personnalisée permettant de créer un formulaire de champs de texte en 
+précisant le nom des champs associés.
+Exemple : <br>
+$nomsInput = array("Nom","Prénom","Ville");<br>
+afficherInput($nomsInput);<br>
+
+<h2>Résultat</h2>
+
+
+<?php
+
