851cab46709c25d3032cd2a750920451eadb31de
JefG67 <jefguilpain@gmail.com>
2024-01-30 22:15:52 +0100

    Exo 11

--- Exo11.php
+++ Exo11.php
@@ -13,13 +13,13 @@ formaterDateFr("2018-02-23");</p>
 
 
 function formaterDateFr($datetrad) {
-    $date = new DateTime($datetrad);
-    $dateenlettre = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE,);
-    echo $dateenlettre->format($date);
+    $date = new DateTime($datetrad); /*Crée un objet DateTime à partir de la date fournie.*/
+    $dateenlettre = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE,); /* Crée un objet IntlDateFormatter configuré pour formater les dates en français, avec le format de date complet et sans format d'heure*/
+    echo $dateenlettre->format($date); /*Utilise l'objet IntlDateFormatter pour formater la date en une chaîne lisible en français et l'affiche.*/
     
  
 }
-
+// la fonction formaterDateFr prend une date, la convertit en objet DateTime, puis la formate en français à l'aide de la classe IntlDateFormatter, et affiche le résultat.
 
 echo formaterDateFr("2018-02-23");
 
